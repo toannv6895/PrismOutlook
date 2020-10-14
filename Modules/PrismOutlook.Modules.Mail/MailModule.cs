003d39fe6d9dcca351861bc8ca1e3c5d450f6718
Toan Nguyen <toannv6895@gmail.com>
2020-10-14 23:27:08 +0700

    tạo giao diện outlook cơ bản và region manager

--- Modules/PrismOutlook.Modules.Mail/MailModule.cs
+++ Modules/PrismOutlook.Modules.Mail/MailModule.cs
@@ -2,14 +2,21 @@
 using Prism.Ioc;
 using Prism.Modularity;
 using Prism.Regions;
+using PrismOutlook.Core;
 
 namespace PrismOutlook.Modules.Mail
 {
     public class MailModule : IModule
     {
+        private readonly IRegionManager _regionManager;
+
+        public MailModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
         public void OnInitialized(IContainerProvider containerProvider)
         {
- 
+            _regionManager.RegisterViewWithRegion(RegionNames.ContentRegion, typeof(ViewA));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
--- PrismOutlook/Views/MainWindow.xaml.cs
+++ PrismOutlook/Views/MainWindow.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System.Windows;
+﻿using Infragistics.Windows.Ribbon;
+using System.Windows;
 
 namespace PrismOutlook.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : XamRibbonWindow
     {
         public MainWindow()
         {
