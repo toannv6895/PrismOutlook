246b842497ad5d6aa88cba9a0965fd74b03bc92e
toannguyen6895 <toannguyen6895@gmail.com>
2020-10-14 22:37:15 +0700

    create new solution

---
+++ Modules/PrismOutlook.Modules.Mail/MailModule.cs
@@ -0,0 +1,20 @@
+﻿using PrismOutlook.Modules.Mail.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace PrismOutlook.Modules.Mail
+{
+    public class MailModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
---
+++ PrismOutlook/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using PrismOutlook.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+using PrismOutlook.Modules.Mail;
+
+namespace PrismOutlook
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<MailModule>();
+        }
+    }
+}
